51066e6c9dec0f6a3a320c09fc38f09fb359195b
wanzbrayyy <wanzbray@gmail.com>
2025-11-02 01:40:58 +0700

    Add files via upload

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { isAuthenticated } = require('../middleware/authController');
+const { isAuthenticated } = require('../middleware/authMiddleware');
 const upload = require('../config/cloudinary');
 const dashboardController = require('../controllers/dashboardController');
 
