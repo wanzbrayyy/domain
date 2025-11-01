0438e610e0e4d774dadadb4fb793aada698367e5
wanzbrayyy <wanzbray@gmail.com>
2025-11-02 01:06:24 +0700

    Create cloudinary.js

---
+++ config/cloudinary.js/cloudinary.js
@@ -0,0 +1,24 @@
+
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'digitalhost_profiles',
+    format: async (req, file) => 'png', 
+    public_id: (req, file) => `user-${req.user.id}`,
+    transformation: [{ width: 250, height: 250, crop: 'limit' }]
+  }
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
